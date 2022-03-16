eae418ff5a54bd5f719f82742cfe4709220887fe
Rura <joseruravi@hotmail.com>
2022-03-16 17:10:01 -0600

    Añadido logo para navbar, nuevo style para navbar

--- js/navbar.js
+++ js/navbar.js
@@ -3,32 +3,32 @@ let navBarElement = document.getElementById('navbar');
 
 navBarElement.innerHTML = `
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Libros.com</a>
+        <a class="navbar-brand" href="#">
+        <img src="/Assets/Logo/LogoNavbar.png" alt="" width="120" height="96" class="d-inline-block align-text-top">
+      </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="/index.html">Inicio</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+              <li class="nav-item">
+                <a class="nav-link" href="/index.html">Libros</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/index.html">Sobre nosotros</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/index.html">Regístrate</a>
               </li>
             </ul>
             <form class="d-flex">
               <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Buscar</button>
+              <button class="btn" type="submit">Buscar</button>
             </form>
           </div>
         </div>
