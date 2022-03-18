9edcfd44173660aefe8dc36281cacf149e107eb6
Rura <joseruravi@hotmail.com>
2022-03-18 14:07:39 -0600

    Actualizada navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,55 @@
 let navBarElement = document.getElementById('navbar');
 
 
-navBarElement.innerHTML = `
 
-<nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-        <img src="/Assets/Logo/LogoNavbar.png" alt="" width="120" height="96" class="d-inline-block align-text-top">
-      </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="/index.html">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/index.html">Libros</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/index.html">Sobre nosotros</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/index.html">Regístrate</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-              <button class="btn" type="submit">Buscar</button>
-            </form>
+
+function loadNavbar(){
+  let navbarItem = "";
+  navbarItem = `
+  
+  <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+          <img src="/Assets/Logo/LogoNavbar.png" alt="" width="120" height="96" class="d-inline-block align-text-top">
+        </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" href="/index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/index.html">Libros</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/index.html">Sobre nosotros</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/index.html">Regístrate</a>
+                </li>`;
+  
+                if(sessionStorage.getItem("cart") != null){
+                  navbarItem += `
+                  <li class="nav-item">
+                  <a class="nav-link" href="/html/cart.html">Carrito</a>
+                </li>
+                  `;
+                }
+  
+                navbarItem += `
+              </ul>
+              <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn" type="submit">Buscar</button>
+              </form>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+  `;
+  
+  navBarElement.innerHTML = navbarItem;
+}
 
-
-`;
+loadNavbar();
